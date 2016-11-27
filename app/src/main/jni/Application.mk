5705c48fe22243a0c3bed65367a4cc8b50f93dce
Misko <mouse9911@gmail.com>
2016-11-26 17:46:15 -0800

    build both x86 and arm

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,5 +1,5 @@
 #APP_ABI := x86
-APP_ABI :=  armeabi-v7a
+#APP_ABI :=  armeabi-v7a
 #APP_ABI = armeabi armeabi-v7a arm64-v8a x86 x86_64
 SHELL := PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin /bin/bash
 
